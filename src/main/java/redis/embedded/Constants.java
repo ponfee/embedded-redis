ebf17433881d74685e9c3b463312ce5c8d926fce
Ponfee <ponfee.cn@gmail.com>
2023-01-12 00:39:06 +0800

    modify maven wrapper version

--- src/main/java/redis/embedded/Constants.java
+++ src/main/java/redis/embedded/Constants.java
@@ -5,16 +5,17 @@ package redis.embedded;
  *
  * <p>Windows downloads page
  * <ul>
- *   <li><a href="https://github.com/microsoftarchive/redis/releases">Windows-2.x</a></li>
- *   <li><a href="https://github.com/tporadowski/redis/releases">Windows-3.x</a></li>
+ *   <li><a href="https://github.com/microsoftarchive/redis/releases">Windows-2.x+</a></li>
+ *   <li><a href="https://github.com/tporadowski/redis/releases">Windows-4.x+</a></li>
+ *   <li><a href="https://github.com/zkteco-home/redis-windows/releases">Windows-6.x+</a></li>
  * </ul>
  *
  * @author Ponfee
  */
 public class Constants {
 
-    public static final String DEFAULT_WINDOWS_X86     = "redis-server-5.0.14.1-win-amd64.exe";
-    public static final String DEFAULT_WINDOWS_X86_64  = "redis-server-5.0.14.1-win-amd64.exe";
+    public static final String DEFAULT_WINDOWS_X86     = "redis-server-6.2.6-win-amd64.exe";
+    public static final String DEFAULT_WINDOWS_X86_64  = "redis-server-6.2.6-win-amd64.exe";
 
     public static final String DEFAULT_UNIX_X86        = "redis-server-6.2.7-linux-386";
     public static final String DEFAULT_UNIX_X86_64     = "redis-server-6.2.7-linux-amd64";
